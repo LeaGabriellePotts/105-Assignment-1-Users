eb80f53299115c3da632cdc7c74931e640b7351c
LeaGabriellePotts <115514366+LeaGabriellePotts@users.noreply.github.com>
2022-11-22 22:14:44 -0500

    105-Assignment-3-Users

--- scripts/register.js
+++ scripts/register.js
@@ -19,12 +19,52 @@ function register(){
     let inputPayment = $("#selPayment").val();
     let inputColor = $("#selColor").val();
 
+    //validations
+    if(!inputFirstName || !inputLastName){
+        console.error("Invalid data");
+        return; //get out of the fn, do not continue
+}
+
     let newUser = new User(inputFirstName,inputLastName,inputAge,inputGender,inputPhone,inputPayment,inputColor);
     console.log(newUser);
 
-    console.log(inputFirstName,inputLastName,inputAge,inputGender,inputPhone,inputPayment,inputColor);
-
+    saveUser(newUser);
+    console.log("User saved!!");
+    
+    clearForm();
 }
+
+function clearForm(){
+$("#txtFirstName").val("");
+$("#txtLastName").val("");
+$("#txtAge").val("");
+$("#txtGender").val("");
+$("#txtPhone").val("");
+$("#selPayment").val("");
+$("#selColor").val("#000000");
+}
+
+
+
+
+
+
+
+function init(){
+    //html is ready
+    console.log("Page ready!");
+    
+    //load data
+    
+    //asigns events
+$("#btnSave").click(register);
+}
+
+//wait for html to be rendered
+window.onload = init;
+
+
+
 
 // //javascript
 // document.getElementById("txtAge").style.display="none";//hiding the element
@@ -32,3 +72,4 @@ function register(){
 // $("#txtAge").hide();
 // So these are the same, first is jquery and second is js:
 // $("txtAge").val();//document.getElementbyId("txtAge").value;
+
